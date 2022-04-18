8f0710ea5527c5195ad66a058ba8eaa58ec620b0
Kassem <systemry420@gmail.com>
2022-04-18 19:25:11 +0300

    configure express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hell ad");
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.get('/', (request: Request, response: Response) => {
+    response.send('Hello from server')
+})
+
+app.listen(8000, () => {
+    console.log('listening to port 8000');
+    
+})
