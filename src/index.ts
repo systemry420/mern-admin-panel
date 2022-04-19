c4d9aa56fd97f89f21c6485c7ec3a8e80d49ce1f
Kassem <systemry420@gmail.com>
2022-04-19 22:58:51 +0300

    connect to mysql

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { routes } from './routes'
+import { createConnection } from 'typeorm'
 
-const app = express()
-
-app.use(express.json())
-
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
-
-routes(app)
-
-app.listen(8000, () => {
-    console.log('listening to port 8000');
+createConnection({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'mysqlmysql123',
+    database: 'node_admin_db',
+    entities: [
+        'src/entity/*.ts'
+    ],
+    synchronize: true
+}).then(connection => {
+    const app = express()
+    
+    app.use(express.json())
+    
+    app.use(cors({
+        origin: 'http://localhost:3000'
+    }))
+    
+    routes(app)
+    
+    app.listen(8000, () => {
+        console.log('listening to port 8000');
+        
+    })
     
 })
